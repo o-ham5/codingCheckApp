cde8182e0dece4f31ecdbcc7f9b57df1d6acf12b
haaam <haaam-blog@gmail.com>
2020-04-18 18:33:56 +0900

    20200418 2

--- codingCheckApp/views.py
+++ codingCheckApp/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.decorators import login_required
 
-from .models import Post, upload_date
+from .models import Post, method_list
 from .forms import SubmitCodeForm
 
 import subprocess
@@ -26,6 +26,7 @@ def PostDetail(request, pk):
     form = SubmitCodeForm()
     user = request.user
     
+    # コード提出後
     if request.method == "POST":
         form = SubmitCodeForm(request.POST, request.FILES)
         if form.is_valid():
@@ -33,9 +34,14 @@ def PostDetail(request, pk):
             # book = form.save(commit=False)
             # book.save()
 
+            # ファイル保存先
             file = request.FILES['file']
-            file_path = f"codes/{user}/{str(datetime.date.today())}/{datetime.datetime.now().time()}/{file.name}"
+            today = str(datetime.date.today())
+            today_time = str(datetime.datetime.now().time()).replace(':', '.')
+            file_path = f"codes/{user}/{today}/{today_time}/{file.name}"
 
+            # {project_ROOT}/media/codes/{username}/{today}/{now}/{filename}としてここで直接保存 (models.pyでは定義していない)
+            # 例: media/codes/admin/2020-01-01/07.42.44.400874/tmp.py
             fileobject = FileSystemStorage()
             fileobject.save(file_path, file)
 
@@ -44,6 +50,7 @@ def PostDetail(request, pk):
             judges = []
             result_outputs = []
 
+            # サブプロセスにて実行
             for k, inp in enumerate(inputs):
                 inp = inp.split("\n")
                 exe_cmd_shell = "("
@@ -54,11 +61,16 @@ def PostDetail(request, pk):
                 stdout_data, stderr_data = p.communicate()
                 stdout_data, stderr_data = stdout_data.decode("utf8").strip(), stderr_data.decode("utf8").strip()
                 result_output = stdout_data
-                judge = outputs[k] == result_output
+
+                # 結果処理
+                if post.method == method_list[0][0]:  # equality
+                    judge = outputs[k] == result_output
+                elif post.method == method_list[1][0]:  # estimation
+                    judge = float(outputs[k]) - float(result_output) < post.tolerable_error
                 judges.append(judge)
                 if not judge and stderr_data:
                     result_output = stderr_data
                 result_outputs.append(result_output)
 
             return render(request, "codingCheckApp/submit_code.html", {'post': post, 'results': enumerate(judges), 'outputs': result_outputs, 'user': user})
-    return render(request, 'codingCheckApp/post_detail.html', {'post': post, 'form': form, 'user': user})
+    return render(request, 'codingCheckApp/post_detail.html', {'post': post, 'form': form, 'user': user,})
